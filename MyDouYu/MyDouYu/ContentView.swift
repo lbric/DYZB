b44cc645313416dcd8a9b79cd143d888e4e55113
lbric <lindaojian@csair.com>
2020-08-23 13:17:05 +0800

    this version from the local

---
+++ MyDouYu/MyDouYu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyDouYu
+//
+//  Created by lbric on 2020/8/23.
+//  Copyright © 2020 lbric. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
